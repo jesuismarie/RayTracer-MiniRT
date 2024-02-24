3ac159d861cb15a13f56210e8a389c42d4023246
Marie Nazaryan <mari.nazaryan7173@gmail.com>
2024-02-24 21:26:31 +0400

    Incs

---
+++ includes/minirt.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnazarya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 16:05:56 by mnazarya          #+#    #+#             */
+/*   Updated: 2024/02/24 20:37:59 by mnazarya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <mlx.h>
+# include <math.h>
+# include <libft.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <ft_printf.h>
+# include <structures.h>
+
+#endif
