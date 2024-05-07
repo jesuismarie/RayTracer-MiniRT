d5af94979ef2e5ea185e49712ca7b9859f0405c3
Gevorg Hovhannisyan <gehovhan@c2r1s3.42yerevan.am>
2024-05-07 23:17:26 +0400

    Start Parsing.

--- sources/minirt.c
+++ sources/minirt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mnazarya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gehovhan <gehovhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 18:42:40 by mnazarya          #+#    #+#             */
-/*   Updated: 2024/03/31 16:28:53 by mnazarya         ###   ########.fr       */
+/*   Updated: 2024/05/07 22:44:13 by gehovhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	main(int argc, char **argv)
 	t_scene	*scene;
 
 	error_exit(argc != 2, "Wrong count of arguments");
+	error_exit(ft_parsing(), "Wrong count of arguments");
 	win_init(argv, &scene);
 	img_init(scene);
 	mlx_hook(scene->mlx->mlx_win, 2, 1l, &keys, scene);
