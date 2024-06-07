21ebc230df059b496c6eb2742a9d9ad0f438675a
kali-linux <mari.nazaryan7173@gmail.com>
2024-06-07 13:12:17 +0400

    libft

--- lib/libft/sources/ft_strncmp.c
+++ lib/libft/sources/ft_strncmp.c
@@ -19,9 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (!s1 && !s2)
 		return (0);
-	if (!s1 || !s2)
-		return (-1);
-	while ((*s1 == *s2) && i < n)
+	while (s1 && s2 && (*s1 == *s2) && i < n)
 	{
 		s1++;
 		s2++;
