885064e13476326a8b00d0758fb118ac45129a7e
kali-linux <mari.nazaryan7173@gmail.com>
2024-06-12 18:40:01 +0400

    lib norm

--- lib/libft/sources/ft_custom_gnl.c
+++ lib/libft/sources/ft_custom_gnl.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_custom_gnl.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gehovhan <gehovhan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mnazarya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 20:28:37 by gehovhan          #+#    #+#             */
-/*   Updated: 2024/05/14 20:28:50 by gehovhan         ###   ########.fr       */
+/*   Updated: 2024/06/12 18:34:41 by mnazarya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ char	*gnl_get_line(char *buffer)
 	index = 0;
 	while (buffer && buffer[index] && buffer[index] != '\n')
 		++index;
-	if (!buffer || !(line = malloc((index + 1))))
+	line = malloc((index + 1));
+	if (!buffer || !line)
 		return (NULL);
 	index = 0;
 	while (buffer && buffer[index] && buffer[index] != '\n')
@@ -35,7 +36,6 @@ char	*gnl_get_line(char *buffer)
 char	*gnl_get_buffer(char *buffer)
 {
 	char	*change;
-	size_t	size;
 	int		index;
 	int		c_index;
 
@@ -49,8 +49,8 @@ char	*gnl_get_buffer(char *buffer)
 		free(buffer);
 		return (NULL);
 	}
-	size = ft_gnl_strlen(buffer) - index;
-	if (!(change = malloc(sizeof(char) * (size + 1))))
+	change = malloc(sizeof(char) * (ft_gnl_strlen(buffer) - index + 1));
+	if (!change)
 		return (NULL);
 	++index;
 	c_index = 0;
@@ -61,32 +61,80 @@ char	*gnl_get_buffer(char *buffer)
 	return (change);
 }
 
-int		ft_getline(int fd, char **line)
+int	ft_gnl_read_loop(t_getline_helper *helper)
 {
-	int				read_return;
-	char			*buffer;
-	static	char	*fd_buffer[1024] = {0};
+	char	*buffer;
 
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
 		return (-1);
-	if (!(buffer = malloc(BUFFER_SIZE + 1)))
-		return (-1);
-	while ((read_return = read(fd, buffer, BUFFER_SIZE)) > 0)
+	while (1)
 	{
-		buffer[read_return] = '\0';
-		fd_buffer[fd] = ft_gnl_strjoin(fd_buffer[fd], buffer);
-		if (ft_gnl_strchr(fd_buffer[fd], '\n') || ft_gnl_strchr(fd_buffer[fd], '\0'))
+		helper->read_return = read(helper->fd, buffer, BUFFER_SIZE);
+		if (helper->read_return <= 0)
+			break ;
+		buffer[helper->read_return] = '\0';
+		*helper->current = ft_gnl_strjoin(*helper->current, buffer);
+		if (ft_gnl_strchr(*helper->current, '\n') \
+			|| ft_gnl_strchr(*helper->current, '\0'))
 			break ;
 	}
 	free(buffer);
-	if (read_return < 0)
+	if (helper->read_return < 0)
+		return (-1);
+	return (helper->read_return);
+}
+
+int	ft_getline(int fd, char **line)
+{
+	t_getline_helper	helper;
+	static char			*fd_buffer[1024] = {0};
+
+	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+		return (-1);
+	if (ft_gnl_read_loop(&helper) < 0)
 		return (-1);
 	*line = gnl_get_line(fd_buffer[fd]);
 	fd_buffer[fd] = gnl_get_buffer(fd_buffer[fd]);
-	if (!read_return && !*line)
+	if (!helper.read_return && !*line)
 	{
 		free(fd_buffer[fd]);
 		return (0);
 	}
 	return (1);
 }
+
+// int	ft_getline(int fd, char **line)
+// {
+// 	int			read_return;
+// 	char		*buffer;
+// 	static char	*fd_buffer[1024] = {0};
+
+// 	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+// 		return (-1);
+// 	buffer = malloc(BUFFER_SIZE + 1);
+// 	if (!buffer)
+// 		return (-1);
+// 	while (1)
+// 	{
+// 		read_return = read(fd, buffer, BUFFER_SIZE);
+// 		if (read_return <= 0)
+// 			break ;
+// 		buffer[read_return] = '\0';
+// 		fd_buffer[fd] = ft_gnl_strjoin(fd_buffer[fd], buffer);
+// 		if (ft_gnl_strchr(fd_buffer[fd], '\n') \
+// 			|| ft_gnl_strchr(fd_buffer[fd], '\0'))
+// 			break ;
+// 	}
+// 	free(buffer);
+// 	if (read_return < 0)
+// 		return (-1);
+// 	*line = gnl_get_line(fd_buffer[fd]);
+// 	fd_buffer[fd] = gnl_get_buffer(fd_buffer[fd]);
+// 	if (!read_return && !*line)
+// 	{
+// 		free(fd_buffer[fd]);
+// 		return (0);
+// 	}
+// 	return (1);
+// }
