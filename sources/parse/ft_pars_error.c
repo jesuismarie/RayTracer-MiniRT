61dac768f20710d8761b67c5e8858f5acde90fc0
Marie <mari.nazaryan7173@gmail.com>
2026-05-14 14:19:50 +0400

    cleanup

--- sources/parse/ft_pars_error.c
+++ sources/parse/ft_pars_error.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pars_error.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mnazarya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mnazarya <mnazarya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 21:54:13 by gehovhan          #+#    #+#             */
-/*   Updated: 2024/06/12 23:27:27 by mnazarya         ###   ########.fr       */
+/*   Updated: 2026/05/13 17:43:18 by mnazarya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minirt.h"
+#include <minirt.h>
 
 bool	set_error(char **error, char *new_error)
 {
